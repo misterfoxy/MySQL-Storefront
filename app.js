f6237b06021515016c04577200a6a20a28c63318
Michael Fox <michaelscottfox1@gmail.com>
2017-10-06 00:31:13 -0700

    customer function now updates stock_quantity from database with user input

--- app.js
+++ app.js
@@ -10,12 +10,16 @@ let connection = mysql.createConnection({
   database: 'bamazon'
 });
 
-connection.connect(
-  console.log("Welcome to Bamazon!")
-);
+connection.connect(function(err){
+  if(err){
+    throw err;
+  }
+
+  console.log("Welcome to Bamazon!");
+
 
 inquirer.prompt([{
-  name:'command',
+  name: 'command',
   message: 'What view would you prefer?',
   type: 'list',
   choices: [{
@@ -25,25 +29,99 @@ inquirer.prompt([{
   }, {
     name: 'Supervisor-View'
   }]
-}]).then(function(answer){
-  if(answer.command === 'Customer-View'){
+}]).then(function(answer) {
+  if (answer.command === 'Customer-View') {
     customerView();
   }
 });
 
-function customerView(){
-  connection.query('SELECT * FROM products', function(err,res){
-    if(err){
+function customerView() {
+  connection.query('SELECT * FROM products', function(err, res) {
+    if (err) {
       throw err;
     }
-    for(var i = 0; i< res.length; i++){
+    // display information for the items for sale
+    for (var i = 0; i < res.length; i++) {
       console.log('--------------------');
       console.log("PRODUCT NAME: " + res[i].product_name);
       console.log("ITEM ID: " + res[i].item_id);
-      console.log("PRICE: " + res[i].item_price);
+      console.log("PRICE: " + res[i].price);
+      console.log("Quantity: " + res[i].stock_quantity);
     }
+    // prompt the user to choose an item id, as well as a quantity
+    inquirer.prompt([{
+      name: 'idNum',
+      type: 'input',
+      message: 'What item would you like to purchase? Please enter item ID to select',
+      validate: function(input) {
+        if (input === '') {
+          console.log('Try again!')
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }, {
+      name: 'quantity',
+      type: 'input',
+      message: 'How many of these would you want?',
+      validate: function(input) {
+        if (input === '') {
+          console.log('Try again!')
+          return false;
+        } else {
+          return true;
+        }
+      }
+    }]).then(function(answer) {
+      let idNum = answer.idNum;
+      let quantity = parseInt(answer.quantity);
 
-    Console.log("continue prompts here");
+      let queryStr = 'SELECT * FROM products WHERE ?';
 
+      connection.query(queryStr, {item_id: idNum}, function(err, results, field) {
+          if (err) {
+            throw err;
+          }
+
+          let productData = results[0];
+
+            if (quantity > productData.stock_quantity) {
+              console.log("Insufficient quantity!");
+
+            }
+
+            else{
+              console.log('Your order will now be completed. The warehouse will be informed.');
+              let newTotal = productData.stock_quantity - quantity;
+
+
+              connection.query("UPDATE products SET ? WHERE ?",[
+                {
+                  stock_quantity: newTotal
+                },
+                {
+                  item_id: idNum
+                }
+              ], function(err, results){
+                if(err){
+                  throw err;
+                }
+
+                connection.query('SELECT item_id FROM products WHERE ?', {item_id: idNum}, function(err, res){
+                  if (err) {
+                    throw err
+                  }
+
+                  console.log(res);
+
+                });
+              });
+              connection.end();
+            }
+      });
+    });
   });
+
 }
+});
